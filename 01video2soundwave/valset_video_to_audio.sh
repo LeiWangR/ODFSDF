b1330370822ca5a000cc4da9f7c9d8cde40c1d78
Lei Wang <u7014095@anu.edu.au>
2023-07-16 21:51:48 +1000

    Add files via upload

---
+++ 01video2soundwave/valset_video_to_audio.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+# follow the params settings in NIPS2016 paper
+# -i is the path to the input file
+# -f specifies the output format is mp3
+# -ac is the audio channel (single channel)
+# -ar is the sample rate (22 kHz)
+# vn means no video
+
+DATASET="validation/*"
+AUDIOSET="validation_audio/"
+for f in $DATASET
+do
+	actionfolder=$(basename -- $f)
+	newfolder="$AUDIOSET$actionfolder"
+	mkdir $newfolder
+	if [ -d "$f" ]
+	then
+		for ff in $f/*
+		do
+			videoname=$(basename -- $ff)
+			# echo $videoname
+			videonameonly=${videoname%.*}	
+			# echo $videonameonly
+			newfilename="$newfolder/$videonameonly.wav"
+			# newfilename="$newfolder/$videonameonly.mp3"
+			# echo $newfilename
+			echo "processing $newfilename"
+			ffmpeg -i $ff -ac 1 -ar 44100 -vn $newfilename
+		done
+		
+	fi
+done
+
+
+
+
